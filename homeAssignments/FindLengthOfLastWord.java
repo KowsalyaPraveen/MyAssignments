b0d8e970e497f6f24b8ff7a0deee91121cd44e61
KowsalyaPraveen <kowsykowsalya24@gmail.com>
2026-01-23 18:59:41 +0530

    Add files via upload
    Uploaded Home Assignments

--- homeAssignments/FindLengthOfLastWord.java
+++ homeAssignments/FindLengthOfLastWord.java
@@ -1,12 +1,12 @@
-package homeAssignments;
+package assignments;
 
-public class FindLengthOfCharacters {
+public class FindLengthOfLastWord {
 
 	public static void main(String[] args) {
-		String sentence1 = "Hello World";
+		String sentence1= "Hello World";
 		String sentence2 = "fly me to the moon";
 		String sentence3 = "luffy is still joyboy";
-		System.out.println("Given sentence: " + sentence3);
+		System.out.println("Given sentence: "+ sentence3);
 
 		String[] wordsArray = sentence3.split(" ");
 		int length = wordsArray.length;
@@ -15,7 +15,7 @@ public class FindLengthOfCharacters {
 		for (int i = 0; i < wordsArray.length; i++) {
 			if (i == length - 1) {
 				int lastWordLength = wordsArray[i].length();
-				System.out.println("Last word " + wordsArray[i] + " has " + lastWordLength + " characters");
+				System.out.println("Last word "+wordsArray[i] + " has " + lastWordLength +" characters");
 			}
 
 		}
